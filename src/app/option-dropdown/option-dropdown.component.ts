0e6484a9dc4c6927961cd1b5bd477d0f9e180c47
MacApos <maciejapostol98@gmail.com>
2025-05-29 03:19:37 +0200

    sort option

--- src/app/option-dropdown/option-dropdown.component.ts
+++ src/app/option-dropdown/option-dropdown.component.ts
@@ -12,38 +12,29 @@ import {NgForOf, NgStyle} from "@angular/common";
     template: `
 
         <div class="position-fixed bottom-0 end-0 my-3 z-3" id="option">
-            <div class="position-relative d-flex justify-content-center align-items-center" id="option-container">
+            <div class="position-relative d-flex justify-content-center align-items-center" id="option-container"
+                 (mouseleave)="overflow=false">
                 <app-button-component icon="arrow-left"
                                       [attributes]="{disabled:'disabled', class:'btn-info opacity-100'}"
                                       id="option-arrow"/>
                 <div class="position-absolute z-n1 d-flex gap-2 justify-content-start align-items-center bg-danger"
                      [ngStyle]="{'overflow': overflow ? 'visible' : 'hidden'}" id="option-dropdown">
                     <app-button-component icon="list" [attributes]="{ class:'btn-success'}" id="display"/>
-                    <div class="d-flex justify-content-center align-items-end gap-2 position-relative bg-info w-100"
-                         id="sort"
-                    >
-                        <!--                         (mouseenter)="overflow=true" (mouseleave)="overflow=false"-->
-
+                    <div class="d-flex justify-content-center align-items-end gap-2 position-relative"
+                         id="sort" (mouseenter)="overflow=true">
                         <app-button-component icon="funnel-fill" [attributes]="{ class:'btn-success'}"/>
-                            <ul class="m-0 position-absolute end-100 list-group" id="sort-option">
-                                <li class="list-group-item list-group-item-action list-group-item-warning"
-                                    *ngFor="let item of [1,2,3,4]">
-                                    {{ "item" + item }}
-                                </li>
-                            </ul>
+                        <ul class="m-0 position-absolute  list-group" id="sort-option">
+                            <li class="list-group-item list-group-item-action list-group-item-warning"
+                                *ngFor="let item of [1,2,3,4]">
+                                {{ "item" + item }}
+                            </li>
+                        </ul>
                     </div>
-                    <!--                    <div class="d-flex bg-info position-absolute top-0 end-0" id="sort">-->
-                    <!--                        <ul class="m-0 bg-warning " >-->
-                    <!--                            <li>sort1</li>-->
-                    <!--                            <li>sort2</li>-->
-                    <!--                        </ul>-->
-                    <!--                        <app-button-component icon="funnel-fill" [attributes]="{ class:'btn-success'}"/>-->
-                    <!--                    </div>-->
                 </div>
             </div>
         </div>
     `,
 })
 export class OptionDropdownComponent {
-    overflow=true
+    overflow=false
 }
